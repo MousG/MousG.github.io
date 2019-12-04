ca9e7587fb913b90b2f4eeb1e5d4b9e053746e74
MousDG <georgejrluciano@gmail.com>
2019-12-04 14:20:30 -0400

    creamos avanza

---
+++ js/demo/klk.js
@@ -0,0 +1,34 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChartMDG");
+var myPieChartMDG = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["A favor", "Indecisos", "En contra"],
+    datasets: [{
+      data: [90234, 5032, 18198],
+      backgroundColor: ['#1cc88a', '#f6c23e', '#e74a3b'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
